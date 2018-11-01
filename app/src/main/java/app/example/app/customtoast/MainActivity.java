9c6434198c5693105933b5d8bc3a28a5e0f609a9
Muthuraja <muthuraja.g@newgendigital.com>
2018-11-01 14:15:28 +0530

    custom toast added

--- app/src/main/java/app/example/app/customtoast/MainActivity.java
+++ app/src/main/java/app/example/app/customtoast/MainActivity.java
@@ -2,12 +2,42 @@ package app.example.app.customtoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btnToast);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup)findViewById(R.id.toast_layout));
+
+                txt = (TextView) layout.findViewById(R.id.text);
+                txt.setText("This is an Custom Toast");
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.FILL_HORIZONTAL|Gravity.BOTTOM , 0 , 0);
+                toast.setView(layout);
+                toast.show();
+
+            }
+        });
     }
 }
